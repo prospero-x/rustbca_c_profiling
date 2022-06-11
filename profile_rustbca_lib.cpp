595dc1119339ce6fc16a5bfcd193599d12725d48
Mikhail Rezazadeh <mrezazade309@gmail.com>
2022-06-11 17:57:51 -0500

    use a meaningful angle for incident particles

--- profile_rustbca_lib.cpp
+++ profile_rustbca_lib.cpp
@@ -1,16 +1,18 @@
 #include "RustBCA.h"
 #include <stdio.h>
 #include <chrono>
+#include <cmath>
 
 
 using std::chrono::high_resolution_clock;
 using std::chrono::duration;
 
 
+
 int main() {
     // All incident particles have the same energy
-    uintptr_t nincident = 3000;  // CHANGEME
-    double E_eV = 500; // CHANGEME
+    uintptr_t nincident = 8000;  // CHANGEME
+    double E_eV = 385;            // CHANGEME
 
     double amu = 1.66054e-27;
 
@@ -24,13 +26,12 @@ int main() {
 
     double qe = 1.602e-19;
 
-    double E_J = E_eV * qe;
 
     // Target particles: Molybdenum
     uintptr_t num_species_target = 1;
     double Z2[1] = {42};
     double m2[1] = {95.95};  // AMU
-    double n2[1] = {0.0423}; // at./A^3
+    double n2[1] = {0.06453}; // at./A^3
     double Ec2[1] = {3.0};   // eV
     double Es2[1] = {6.81};  // eV
     double Eb2[1] = {3.0};   // eV
@@ -41,6 +42,18 @@ int main() {
     double (*weights)       = new double[nincident];
     int32_t (*tags)         = new int32_t[nincident];
 
+    // Calculate particle velocity from energy
+    double E_J = E_eV * qe;
+    double vmag = sqrt(2*E_J/mi_kg);
+    printf("|v|: %.2f km/s\n", vmag/1000);
+
+    // The particles can all have the same direction but must have significant
+    // non-zero vy
+    double theta_deg = 45;
+    double vx = -vmag * cos(theta_deg / 180 * M_PI);
+    double vy = -vmag * sin(theta_deg/ 180 * M_PI) / 2;
+    double vz = sqrt(vmag*vmag - vx*vx - vy*vy);
+
     for (int i = 0; i < nincident; ++i) {
         // incident particle position
         positions[i][0] = 0.001;
@@ -48,9 +61,9 @@ int main() {
         positions[i][0] = 0;
 
         // incident particle velocity. Make direct hit.
-        velocities[i][0] = sqrt(2*E_J/mi_kg);
-        velocities[i][1] = 0;
-        velocities[i][2] = 0;
+        velocities[i][0] = -7.3647e+03;
+        velocities[i][1] = -6.8778e+04;
+        velocities[i][2] = 1.4666e+04;
 
         weights[i] = 1e10;
         tags[i]    = i;
